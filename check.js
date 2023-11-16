90ff309b55e6ef571f1cff2000c4f4f214cef050
Rpise <munashegaming@gmail.com>
2023-11-16 18:58:59 +0200

    Update github checks to be correct

--- check.js
+++ check.js
@@ -11,10 +11,14 @@ async function startSystem() {
     client.login(config.token).then(() => {
         console.log("(!) Connected to Discord as " + client.user.username + "!");
         console.log("[Create Server] Creating server...")
-        require('./api/v1/index')(client);
+        require('./api/index')(client);
       }).catch(err => {
-        console.error(err+"Error occured while logging in");
-      });
+        console.error("(!) "+err+"\n An error occured while loading the servers. Please fix before merging it into the production branch.");
+        process.exit(1);
+      }).catch((err) => {
+          console.error("(!) "+err+"\n An error occured while logging in to discord. Please fix before merging it into the production branch.");
+          process.exit(1);
+      })
       console.log("DIM API \n Loading API Module - Database")
     await connect(config.mongoURL, {
         useNewUrlParser: true,
